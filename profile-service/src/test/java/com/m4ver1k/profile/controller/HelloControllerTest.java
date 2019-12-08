8a65db48819e89cf616d4375acd71d97722d1bbd
M4ver1k <adarshkkumar@gmail.com>
2019-12-08 19:25:00 +0530

    #2 | Add hello world api in backend | Adarsh

---
+++ profile-service/src/test/java/com/m4ver1k/profile/controller/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package com.m4ver1k.profile.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void greetingShouldReturnMessageFromService() throws Exception {
+
+        this.mockMvc.perform(get("/hello")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+}
